a22e4982ccb9a5d174bd90602c6a698a22a97624
Karthik <220030807+FlightElevate@users.noreply.github.com>
2026-08-03 13:23:11 +0530

    Update Bar fill colors in FSession component

--- src/components/Dashboard/FSession.jsx
+++ src/components/Dashboard/FSession.jsx
@@ -236,7 +236,7 @@ const FSession = () => {
             <Bar
               dataKey="single"
               name="Single Engine"
-              fill="#A5F3FC"
+              fill="#14B8A6"
               shape={<CustomBarShape />}
             />
 
@@ -244,7 +244,7 @@ const FSession = () => {
             <Bar
               dataKey="multi"
               name="Multi Engine"
-              fill="#1D4ED8"
+              fill="#6366F1"
               shape={<CustomBarShape />}
             />
           </BarChart>
