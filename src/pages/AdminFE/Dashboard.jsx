06edcadc7e2703583a7126961935852db094f11a
Karthik <220030807+FlightElevate@users.noreply.github.com>
2026-08-03 19:05:29 +0530

    Adjust layout padding in Dashboard component

--- src/pages/AdminFE/Dashboard.jsx
+++ src/pages/AdminFE/Dashboard.jsx
@@ -13,7 +13,7 @@ const AdminDashboard = React.memo(() => {
   }, [user?.name, user?.first_name]);
 
   return (
-    <div className="p-2 gap-6">
+    <div className="p-2 -mx-4 gap-6">
       <div className='mb-10'>
         <h2 className="text-3xl fw6 leading-[38px]">Welcome Back, {userName}</h2>
         <p className='text-base text-[#8A8A8A]'>Keep track of your flight lesson records and analytics here.</p>
